1c3e89bc7e1c35a7254a4ca21d6b4c200555b875
unknown <saliou barry>
2021-09-19 13:05:16 +0200

    elif-statement

--- metenIsWeten.py
+++ metenIsWeten.py
@@ -1,8 +1,13 @@
 a = int(input('vul het getal hier voor A: '))
 b = int(input('vul het getal hier voor B: '))
 print(type(a))
+
+max = a
+min = a
+
 if a > b:
-    max = a
-    min = b
     print ('a is het grootste getal: ' ,max)
+elif a < b:
+    print('a is het kleinste getal" ' , min)
+
  
