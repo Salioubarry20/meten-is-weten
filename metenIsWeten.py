3a355dfd235919fc8a48897ea6c326faf9cc42c1
unknown <saliou barry>
2021-09-19 12:54:06 +0200

    input en if-statement

---
+++ metenIsWeten.py
@@ -0,0 +1,8 @@
+a = int(input('vul het getal hier voor A: '))
+b = int(input('vul het getal hier voor B: '))
+print(type(a))
+if a > b:
+    max = a
+    min = b
+    print ('a is het grootste getal: ' ,max)
+ 
