db5196a85132bad0af0c4a15c8831012f8e56053
unknown <saliou barry>
2021-09-24 09:38:08 +0200

    e

---
+++ wieIshet.py
@@ -0,0 +1,16 @@
+Question = input("Is de kaas geel?: ")
+if Question == 'ja'
+if Question = 'nee'
+
+
+str(input("Heeft de kaas blauwe schimmels?"))
+
+ja = ("ja de kaas is geel")
+nee = ("nee de kaas is niet geel")
+
+if ja > nee:
+    print("zitten er gaten in?:" , ja)
+
+elif ja < nee:
+    print("Heeft de kaas blauwe schimmels?:" , nee)  
+    ja = ("ja de kaas is geel")
